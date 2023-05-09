a1385fb42cd2df29490b1b874d076e50c0494cf9
Evgeniy Solovev <73435568+djonnybravo@users.noreply.github.com>
2023-05-09 21:49:16 +0300

    add content

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Rating from "./components/Rating/Rating";
 
 function App() {
   return (
     <div className="App">
-
+      <Rating/>
     </div>
   );
 }
--- src/components/Rating/Rating.tsx
+++ src/components/Rating/Rating.tsx
@@ -3,7 +3,11 @@ import React from 'react';
 const Rating = () => {
     return (
         <div>
-
+            <div>star</div>
+            <div>star</div>
+            <div>star</div>
+            <div>star</div>
+            <div>star</div>
         </div>
     );
 };
