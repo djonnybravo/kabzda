4656530286a668cece2193c610da48085cebf5ba
Evgeniy Solovev <73435568+djonnybravo@users.noreply.github.com>
2023-05-09 21:50:35 +0300

    add Accordion.tsx

---
+++ src/components/Accordion/Accordion.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Accordion = () => {
+    return (
+        <div>
+            <h3>Menu</h3>
+            <ul>
+                <li>First</li>
+                <li>Seccond</li>
+                <li>Third</li>
+            </ul>
+        </div>
+    );
+};
+
+export default Accordion;
