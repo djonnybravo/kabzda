4d6245050df59d2df78fa971fc32c4022c3b6b2c
Evgeniy Solovev <73435568+djonnybravo@users.noreply.github.com>
2023-05-09 21:51:36 +0300

    add Accordion

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Rating from "./components/Rating/Rating";
+import Accordion from "./components/Accordion/Accordion";
 
 function App() {
   return (
     <div className="App">
-      <Rating/>
+        <Accordion/>
+        <Rating/>
     </div>
   );
 }
