aab81940dcdb8efcc20d8591cd642b1bbc230a38
djonnybravo <djonnybravo29@gmail.com>
2023-05-10 17:08:06 +0300

    add props

--- src/components/Rating/Rating.tsx
+++ src/components/Rating/Rating.tsx
@@ -8,11 +8,11 @@ type RatingPropsType = {
 const Rating = (props: RatingPropsType) => {
     return (
         <div>
-            <Star selected={props.value === 1}/>
-            <Star selected={props.value === 2}/>
-            <Star selected={props.value === 3}/>
-            <Star selected={props.value === 4}/>
-            <Star selected={props.value === 5}/>
+            <Star selected={props.value > 0}/>
+            <Star selected={props.value > 1}/>
+            <Star selected={props.value > 2}/>
+            <Star selected={props.value > 3}/>
+            <Star selected={props.value > 5}/>
         </div>
     );
 };
