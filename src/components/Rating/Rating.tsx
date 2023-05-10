4fd26ed7abfdcab5f7dd59ec0376d3d604bf9041
djonnybravo <djonnybravo29@gmail.com>
2023-05-10 17:05:26 +0300

    add props

--- src/components/Rating/Rating.tsx
+++ src/components/Rating/Rating.tsx
@@ -2,18 +2,17 @@ import React from 'react';
 import Star from "./Star/Star";
 
 type RatingPropsType = {
-    value: number
+    value: 0 | 1 | 2 | 3 | 4 | 5
 }
 
 const Rating = (props: RatingPropsType) => {
     return (
         <div>
-           <Star selected={true}/>
-           <Star selected={true}/>
-           <Star selected={true}/>
-           <Star selected={false}/>
-           <Star selected={false}/>
-
+            <Star selected={props.value === 1}/>
+            <Star selected={props.value === 2}/>
+            <Star selected={props.value === 3}/>
+            <Star selected={props.value === 4}/>
+            <Star selected={props.value === 5}/>
         </div>
     );
 };
