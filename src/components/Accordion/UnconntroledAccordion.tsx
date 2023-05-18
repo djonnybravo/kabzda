a06ecb273326b2346438c11a6abe5281613d9855
Evgeniy Solovev <73435568+djonnybravo@users.noreply.github.com>
2023-05-18 19:57:22 +0300

    add UnconntroledAccordion.tsx

---
+++ src/components/Accordion/UnconntroledAccordion.tsx
@@ -0,0 +1,24 @@
+import React, {useState} from 'react';
+import AccordionTitle from "./AccordionTitle/AccordionTitle";
+import AccordionBody from "./AccordionBody/AccordionBody";
+
+type UnconntroledAccordionPropsType = {
+    title: string
+}
+
+
+
+const UnconntroledAccordion = (props: UnconntroledAccordionPropsType) => {
+
+    const [collapsed, setCollapsed] = useState(false)
+    return (
+        <div onClick={ () => {setCollapsed(!collapsed)}}>
+            <AccordionTitle title={props.title}/>
+
+                {!collapsed && <AccordionBody/> }
+
+        </div>
+    );
+};
+
+export default UnconntroledAccordion;
