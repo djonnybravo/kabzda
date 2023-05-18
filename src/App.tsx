7b94b2f5a168678fa5a5bdb46b5256566b02528f
Evgeniy Solovev <73435568+djonnybravo@users.noreply.github.com>
2023-05-18 19:57:25 +0300

    add UnconntroledAccordion.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
-import React, {Component, useState} from "react";
-
+import React from "react";
 import "./App.css";
 import Rating from "./components/Rating/Rating";
 import Accordion from "./components/Accordion/Accordion";
 import OnOff from "./components/OnOff/OnOff";
+import UnconntroledAccordion from "./components/Accordion/UnconntroledAccordion";
 
 function App() {
   return (
     <div className="App">
         <Accordion title = {"Meню"} collapsed={true}/>
+        <hr/>
+        <UnconntroledAccordion title={"Uncontrolled"} />
         <br/><br/><hr/>
         <OnOff on={true}/>
         <OnOff on={false}/>
