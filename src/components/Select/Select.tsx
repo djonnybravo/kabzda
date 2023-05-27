ee37195836975f39e137b69e196c7a85b7a516e4
Evgeniy Solovev <73435568+djonnybravo@users.noreply.github.com>
2023-05-27 17:43:03 +0300

    add Controlled Select.tsx

--- src/components/Select/Select.tsx
+++ src/components/Select/Select.tsx
@@ -1,13 +1,19 @@
-import React from 'react';
+import React, {ChangeEvent, useState} from 'react';
 
 const Select = () => {
+    const [value, setValue] = useState<string | undefined>('3')
+
+    const onSelectChange = (e:ChangeEvent<HTMLSelectElement>) => setValue(e.currentTarget.value)
+
     return (
-        <select>
-            <option>none</option>
-            <option value={'1'}>London</option>
-            <option value={'2'}>NewYork</option>
-            <option value={'3'}>Moscow</option>
-        </select>
+        <div>
+            <select value={value} onChange={onSelectChange}>
+                <option>none</option>
+                <option value={'1'}>London</option>
+                <option value={'2'}>NewYork</option>
+                <option value={'3'}>Moscow</option>
+            </select>
+        </div>
     );
 };
 
