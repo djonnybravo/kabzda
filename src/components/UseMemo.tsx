b287c7b317b09a76839e2d29163ad7b7bbc2a1ae
djonnybravo <djonnybravo29@gmail.com>
2023-06-01 23:30:58 +0300

    add useMemo

--- src/components/UseMemo.tsx
+++ src/components/UseMemo.tsx
@@ -27,13 +27,38 @@ const UseMemo = () => {
 
     return (
         <div>
-            <input value={a} onChange={(e) => setA(+e.currentTarget.value) }/>
-            <input value={b} onChange={(e) => setB(Number(e.currentTarget.value)) }/>
-            <hr/>
+            <ReactMEMO/>
+
+
+            <div>
+                <h3>useMemo</h3>
+                <input value={a} onChange={(e) => setA(+e.currentTarget.value) }/>
+                <input value={b} onChange={(e) => setB(Number(e.currentTarget.value)) }/>
+                <hr/>
                 <div>Result for A: {resultA}</div>
                 <div>Result for B: {resultB}</div>
+
+            </div>
+
         </div>
     );
 };
 
 export default UseMemo;
+
+
+const ReactMEMO = () => {
+
+    const [counter, setCounter] = useState(0)
+    const [users, setUsers] = useState(["Sasha", "Dima", "Petya"])
+
+
+
+
+    return <div>
+        <h3>ReactMEMO</h3>
+        <div>
+
+        </div>
+    </div>
+}
